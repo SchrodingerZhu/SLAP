444f75fa8ab1fa38d52cbaa943012d09e13f6f10
Schrodinger ZHU Yifan <i@zhuyi.fan>
2024-10-09 00:32:56 -0400

    [extractor] finish extraction

--- src/extractor/extractor.cpp
+++ src/extractor/extractor.cpp
@@ -1,5 +1,9 @@
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/ValueRange.h"
 #include "mlir/Interfaces/ControlFlowInterfaces.h"
+#include <llvm-20/llvm/ADT/ArrayRef.h>
 #include <llvm/ADT/DenseMap.h>
 #include <llvm/Support/ErrorHandling.h>
 #include <llvm/Support/MemoryBuffer.h>
@@ -17,6 +21,7 @@
 #include <mlir/Parser/Parser.h>
 
 #include <memory>
+#include <numeric>
 #include <slap.h>
 
 namespace {
@@ -32,6 +37,7 @@ void initialize(MLIRContext &context) {
 class ExtractContext {
   slap_context_t slap_ctx;
   llvm::DenseMap<Value, size_t> ivars;
+  llvm::DenseMap<Value, size_t> memrefs;
   affine::AffineForOp parent;
   slap_graph_t parent_epilogue;
   void record_ivar(Value ivar) {
@@ -51,14 +57,21 @@ public:
   size_t getNumOfIvars() { return ivars.size(); }
   size_t getIVar(Value ivar) {
     auto it = ivars.find(ivar);
-    if (it == ivars.end())
+    if (it == ivars.end()) {
       llvm_unreachable("induction variable not found");
+    }
     return it->second;
   }
   slap_graph_t getParentEpilogue() { return parent_epilogue; }
   void setParentCondEpilogque(slap_graph_t parent) { parent_epilogue = parent; }
   affine::AffineForOp getParent() { return parent; }
   void setParent(affine::AffineForOp parent) { this->parent = parent; }
+  size_t getMemRef(Value memref) {
+    auto it = memrefs.find(memref);
+    if (it == memrefs.end())
+      return memrefs[memref] = memrefs.size();
+    return it->second;
+  }
 };
 
 class ParentGuard {
@@ -80,17 +93,17 @@ public:
   }
 };
 
-struct AffineBoundContext {
+struct AffineContext {
   ExtractContext &ext_ctx;
-  affine::AffineBound bound;
   std::optional<AffineExpr> parent;
   llvm::SmallVector<ssize_t> coeff;
   ssize_t bias;
+  OperandRange operands;
 
   size_t getIVar(Value ivar) { return ext_ctx.getIVar(ivar); }
 };
 
-void extractAffineExpr(AffineExpr expr, AffineBoundContext &ctx) {
+void extractAffineExpr(AffineExpr expr, AffineContext &ctx) {
   switch (expr.getKind()) {
   case AffineExprKind::Add: {
     auto add = cast<AffineBinaryOpExpr>(expr);
@@ -107,6 +120,7 @@ void extractAffineExpr(AffineExpr expr, AffineBoundContext &ctx) {
     ctx.parent = expr;
     extractAffineExpr(mul.getRHS(), ctx);
     ctx.parent = old_parent;
+    break;
   }
   case AffineExprKind::Constant: {
     auto constant = cast<AffineConstantExpr>(expr);
@@ -116,7 +130,7 @@ void extractAffineExpr(AffineExpr expr, AffineBoundContext &ctx) {
       auto mul = cast<AffineBinaryOpExpr>(*ctx.parent);
       auto dim = cast<AffineDimExpr>(mul.getLHS());
       auto pos = dim.getPosition();
-      auto value = ctx.bound.getOperand(pos);
+      auto value = ctx.operands[pos];
       ctx.coeff[ctx.getIVar(value)] = constant.getValue();
     }
     break;
@@ -124,7 +138,7 @@ void extractAffineExpr(AffineExpr expr, AffineBoundContext &ctx) {
   case AffineExprKind::DimId: {
     auto dim = cast<AffineDimExpr>(expr);
     auto pos = dim.getPosition();
-    auto value = ctx.bound.getOperand(pos);
+    auto value = ctx.operands[pos];
     ctx.coeff[ctx.getIVar(value)] = 1;
     break;
   }
@@ -141,16 +155,96 @@ slap_expr_t extractAffineExpr(affine::AffineBound expr, ExtractContext &ctx) {
   if (map.getNumResults() != 1)
     llvm_unreachable("only single result affine map is supported");
   auto result = map.getResult(0);
-  auto affine_ctx = AffineBoundContext{
+  auto affine_ctx = AffineContext{
       .ext_ctx = ctx,
-      .bound = expr,
       .parent = std::nullopt,
       .coeff = llvm::SmallVector<ssize_t>(ctx.getNumOfIvars(), 0),
       .bias = 0,
+      .operands = expr.getOperands(),
   };
   extractAffineExpr(result, affine_ctx);
   return slap_expr_new(ctx.getSLAPContext(), affine_ctx.coeff.data(),
                        affine_ctx.coeff.size(), affine_ctx.bias);
+}
+
+slap_graph_t extractOperation(Operation *op, ExtractContext &ctx);
+slap_graph_t extractFromLoop(affine::AffineForOp loop, ExtractContext &ctx);
+
+slap_graph_t extractAffineAccess(Value memref, AffineMap map,
+                                 OperandRange operands, Operation *next_node,
+                                 MLIRContext *mctx, ExtractContext &ctx) {
+  if (auto memTy = dyn_cast<MemRefType>(memref.getType())) {
+    if (!memTy.hasStaticShape())
+      llvm_unreachable("dynamic shape memref is not supported");
+    auto layout = memTy.getLayout();
+    if (layout.isIdentity()) {
+      auto shape = memTy.getShape();
+      auto exprs = map.getResults();
+      auto zip = llvm::zip(shape, exprs);
+      auto acc = std::accumulate(
+          zip.begin(), zip.end(), getAffineConstantExpr(0, mctx),
+          [](AffineExpr acc, std::tuple<int64_t, AffineExpr> pair) {
+            auto [size, expr] = pair;
+            return acc * size + expr;
+          });
+      auto affine_ctx = AffineContext{
+          .ext_ctx = ctx,
+          .parent = std::nullopt,
+          .coeff = llvm::SmallVector<ssize_t>(ctx.getNumOfIvars(), 0),
+          .bias = 0,
+          .operands = operands,
+      };
+      acc = simplifyAffineExpr(acc, map.getNumDims(), map.getNumSymbols());
+      extractAffineExpr(acc, affine_ctx);
+      auto expr = slap_expr_new(ctx.getSLAPContext(), affine_ctx.coeff.data(),
+                                affine_ctx.coeff.size(), affine_ctx.bias);
+      auto memref_id = ctx.getMemRef(memref);
+      auto next = extractOperation(next_node, ctx);
+      return slap_graph_new_access(ctx.getSLAPContext(), memref_id, expr, next);
+    } else if (auto strided = dyn_cast<StridedLayoutAttr>(layout)) {
+      if (!strided.hasStaticLayout())
+        llvm_unreachable("dynamic layout is not supported");
+      auto offset = strided.getOffset();
+      auto stride = strided.getStrides();
+      auto exprs = map.getResults();
+      auto zip = llvm::zip(stride, exprs);
+      auto acc = std::accumulate(
+          zip.begin(), zip.end(), getAffineConstantExpr(offset, mctx),
+          [](AffineExpr acc, std::tuple<int64_t, AffineExpr> pair) {
+            auto [strided, expr] = pair;
+            return acc + strided * expr;
+          });
+      auto affine_ctx = AffineContext{
+          .ext_ctx = ctx,
+          .parent = std::nullopt,
+          .coeff = llvm::SmallVector<ssize_t>(ctx.getNumOfIvars(), 0),
+          .bias = 0,
+          .operands = operands,
+      };
+      extractAffineExpr(acc, affine_ctx);
+      auto expr = slap_expr_new(ctx.getSLAPContext(), affine_ctx.coeff.data(),
+                                affine_ctx.coeff.size(), affine_ctx.bias);
+      auto memref_id = ctx.getMemRef(memref);
+      auto next = extractOperation(next_node, ctx);
+      return slap_graph_new_access(ctx.getSLAPContext(), memref_id, expr, next);
+    }
+    llvm_unreachable("unsupported layout");
+  }
+  llvm_unreachable("unsupported memref type");
+}
+
+slap_graph_t extractAffineAccess(affine::AffineLoadOp load,
+                                 ExtractContext &ctx) {
+  return extractAffineAccess(load.getMemRef(), load.getAffineMap(),
+                             load->getOperands().drop_front(),
+                             load->getNextNode(), load.getContext(), ctx);
+}
+
+slap_graph_t extractAffineAccess(affine::AffineStoreOp store,
+                                 ExtractContext &ctx) {
+  return extractAffineAccess(store.getMemRef(), store.getAffineMap(),
+                             store.getOperands().drop_front(2),
+                             store->getNextNode(), store.getContext(), ctx);
 }
 
 slap_graph_t createEpilogue(affine::AffineForOp loop, ExtractContext &ctx,
@@ -164,9 +258,6 @@ slap_graph_t createEpilogue(affine::AffineForOp loop, ExtractContext &ctx,
   return slap_graph_new_update(ctx.getSLAPContext(), ivar, expr, cond_node);
 }
 
-slap_graph_t extractOperation(Operation *op, ExtractContext &ctx);
-slap_graph_t extractFromLoop(affine::AffineForOp loop, ExtractContext &ctx);
-
 slap_graph_t extractTerminator(Operation *op, ExtractContext &ctx) {
   if (isa<func::ReturnOp>(op))
     return slap_graph_new_end(ctx.getSLAPContext());
@@ -179,8 +270,12 @@ slap_graph_t extractOperation(Operation *op, ExtractContext &ctx) {
   for (;;) {
     if (op->hasTrait<OpTrait::IsTerminator>())
       return extractTerminator(op, ctx);
-    if (isa<affine::AffineForOp>(op))
-      return extractFromLoop(cast<affine::AffineForOp>(op), ctx);
+    if (auto loop = dyn_cast<affine::AffineForOp>(op))
+      return extractFromLoop(loop, ctx);
+    if (auto load = dyn_cast<affine::AffineLoadOp>(op))
+      return extractAffineAccess(load, ctx);
+    if (auto store = dyn_cast<affine::AffineStoreOp>(op))
+      return extractAffineAccess(store, ctx);
     if (isa<RegionBranchOpInterface>(op))
       llvm_unreachable(
           "region branch other than affine for is not supported yet");
